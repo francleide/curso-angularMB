a5819f92bdafbddfab611befad7c4256190c3874
francleide <francleide7@gmail.com>
2023-05-17 19:26:43 -0300

    first commit

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent {
+  name : string ='Fran'; //typescript variavel : tipo (opcional) = valor
+}
